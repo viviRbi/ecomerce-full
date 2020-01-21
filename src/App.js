3dda73e5fc1af7131afc43b454571649b14173f0
VyLe <thaovyletruong@gmail.com>
2020-01-21 16:56:24 -0600

    form when click on image, clicked Id image on state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: []
+      product: [],
+      clicked: ''
     };
   }
   componentDidMount() {
@@ -13,15 +14,31 @@ class App extends Component {
       .then(data => data.json())
       .then(data => this.setState({ product: data }))
   }
+  clickHandle = (e) => {
+    const clicked = e.target.attributes.getNamedItem('id').value
+    this.setState({ clicked })
+  }
 
   render() {
     return (
       <div className="App">
+        <div className={this.state.clicked ? "overlay" : "none"}>
+          <form>
+            <div>
+              <input type="text" placeholder="Name" required />
+              <input type="number" placeholder="Price" required />
+              <input type="text" placeholder="Url" required />
+              <input type="submit" className="submit" />
+            </div>
+          </form>
+        </div>
+
         {this.state.product.map(e => (
-          <div>
-            <h1>{e.name}</h1><button>Delete</button>
+          <div key={e._id}>
+            <button>Delete</button>
+            <img src={e.url} alt={e.url} id={e._id} onClick={e => this.clickHandle(e)} />
+            <h1>{e.name}</h1>
             <h1>{e.price}</h1>
-            <img src={e.url} alt={e.url} />
           </div>
         ))}
         <div>
@@ -30,7 +47,6 @@ class App extends Component {
           <button>Read</button>
         </div>
       </div>
-
     );
   }
 }
