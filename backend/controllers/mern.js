1a89f9505c999e6dbabd64fdec7f8e1d08a9d918
VyLe <thaovyletruong@gmail.com>
2020-01-22 00:45:52 -0600

    put/post/delete immediately

--- backend/controllers/mern.js
+++ backend/controllers/mern.js
@@ -4,17 +4,19 @@ let mernController = {
     Mern.find({}).then(mern => res.json(mern));
   },
   post: (req, res) => {
-    Mern.create(req.body).then(() => res.redirect("/mern"));
+    Mern.create(req.body).then(() => res.redirect(303, "/mern"));
   },
   put: (req, res) => {
     Mern.findOneAndUpdate({ _id: req.params.id }, req.body).then(() => {
       Mern.find().then(mern => res.json(mern))
+        .then(() => res.redirect(303, "/mern"))
     });
   },
   delete: (req, res) => {
-    Mern.deleteOne({ _id: req.params.id}).then(()=>{
+    Mern.deleteOne({ _id: req.params.id }).then(() => {
       Mern.find().then(mern => res.json(mern))
+        .then(() => res.redirect(303, "/mern"))
     });
   }
-}; 
+};
 module.exports = mernController;
